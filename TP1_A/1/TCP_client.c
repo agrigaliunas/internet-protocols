bb97d99890baca02bcde5991f033ae4d59cc6f4d
agrigaliunas <agustingrigaliunas@gmail.com>
2022-04-04 19:13:49 -0300

    TP1A item 1 finished

--- TP1_A/1/TCP_client.c
+++ TP1_A/1/TCP_client.c
@@ -9,60 +9,64 @@
 #include <unistd.h>
 
 #define PORT 3490
-
 #define MAXDATASIZE 100
 
-int main(int argc, char *argv[])
-{
- int sockfd, numbytes;
- char buf[MAXDATASIZE];
- struct hostent *he;
- struct sockaddr_in their_addr;
 
- if (argc != 2 )
- {
-  fprintf(stderr, "usage: client hostname\n");
-  exit(1);
- }
+int main(int argc, char *argv[]){ // argc
 
- if ((he=gethostbyname(argv[1])) == NULL)
- {
-  herror("gesthostbyname");
-  exit(1);
- }
+    int socket_file_descriptor;
+    int bytes_recieved;
+    char messageRecieved[MAXDATASIZE];
 
- if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
- {
-  perror("socket");
-  exit(1);
- }
+    struct hostent *he; // contiene campos con informacion del host
+    struct sockaddr_in their_addr; // guarda info sobre quien se va a conectar
 
- their_addr.sin_family = AF_INET;
- their_addr.sin_port   = htons(PORT);
- their_addr.sin_addr   = *((struct in_addr *)he->h_addr);
- bzero(&(their_addr.sin_zero), 8);
+    if(argc != 2){
+        fprintf(stderr,"Por favor ingrese el hostname del cliente\n");
+        exit(1);
+    }
 
- if (connect(sockfd, (struct sockaddr *)&their_addr, sizeof(struct sockaddr)) == -1)
- {
-  perror("connect");
-  exit(1);
- }
+    // completo la estructura "he" y reviso si esta NULL
 
-while(1)
-{
- if ((numbytes=recv(sockfd, buf, MAXDATASIZE, 0)) == -1)
- {
-  perror("recv");
-  exit(1);
- }
+    if ((he=gethostbyname(argv[1])) == NULL){ // gethostbyname retorna puntero a estructura hostent
+        perror("gethostbyname");
+        exit(1);
+    }
 
- buf[numbytes] = '\0';
+    // AF_INET -> familia de direcciones
+    // SOCK_STREAM -> para decir que es un socket de flujo. TCP/IP
+    if ((socket_file_descriptor = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        perror("socket");
+        exit(1);
+    }
 
- printf("Mensaje recibido: %s\n", buf);
- if (send(sockfd, "Respondiendo del cliente\n", 26, 0) == -1) perror("send");
- else printf("Mensaje enviado\n");
-}
- close(sockfd);
+    their_addr.sin_family = AF_INET;
+    their_addr.sin_port = htons(PORT);
+    their_addr.sin_addr = *((struct in_addr *)he->h_addr);
+    bzero(&(their_addr.sin_zero), 8);
 
- return 0;
+
+    if (connect(socket_file_descriptor, (struct sockaddr *)&their_addr, sizeof(struct sockaddr)) == -1){
+        perror("connect");
+        exit(1);
+    }
+
+    while(1){
+
+        if ((bytes_recieved=recv(socket_file_descriptor, messageRecieved, MAXDATASIZE, 0)) == -1){
+            perror("recv");
+            exit(1);
+        }
+        messageRecieved[bytes_recieved] = '\0';
+
+        printf("Mensaje recibido del server: %s\n", messageRecieved);
+        printf("Bytes recibidos: %ld\n",strlen(messageRecieved)*sizeof(messageRecieved));
+
+
+    }
+
+    close(socket_file_descriptor);
+
+    return 0;
+
 }
--- TP1_A/TCP_server.c
+++ TP1_A/TCP_server.c
@@ -14,68 +14,70 @@
 #define BACKLOG 5 // Cantidad de conecciones en la cola.
 #define MAXDATASIZE 100
 
-main()
-{
- int sockfd, new_fd; // escuchamos en sockfd, nueva coneccion en new_fd
- struct sockaddr_in my_addr; 
- struct sockaddr_in their_addr;
- socklen_t sin_size;
- int numbytes;
- char buf[MAXDATASIZE];
+int main(){
+    int socket_file_descriptor
+    int new_file_descriptor; // escuchamos en sockfd, nueva coneccion en new_fd
+    struct sockaddr_in my_addr; 
+    struct sockaddr_in their_addr;
+    socklen_t sin_size;
+    int bytes_recieved;
+    char message[MAXDATASIZE];
 
 
- if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
- {
-  perror("socket");
-  exit(1);
- }
+    if ((socket_file_descriptor = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        perror("socket");
+        exit(1);
+    }
 
- my_addr.sin_family = AF_INET;
- my_addr.sin_port   = htons(MYPORT);
- my_addr.sin_addr.s_addr = INADDR_ANY;
- bzero(&(my_addr.sin_zero), 8);
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_port   = htons(MYPORT);
+    my_addr.sin_addr.s_addr = INADDR_ANY;
+    bzero(&(my_addr.sin_zero), 8);
 
- if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) == -1)
- {
-  perror("bind");
-  exit(1);
- }
+    if (bind(socket_file_descriptor, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) == -1){
+        perror("bind");
+        exit(1);
+    }
 
- if (listen(sockfd, BACKLOG) == - 1)
- {
-  perror("listen");
-  exit(1);
- }
+    if (listen(socket_file_descriptor, BACKLOG) == - 1){
+        perror("listen");
+        exit(1);
+    }
 
- while(1)
- {
-  sin_size = sizeof(struct sockaddr_in);
-  if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size)) == -1)
-  {
-   perror("accept");
-   continue;
-  }
-  printf("server: got connection from %s\n", inet_ntoa(their_addr.sin_addr));
+    while(1){
+        sin_size = sizeof(struct sockaddr_in);
+        
+        if ((new_file_descriptor = accept(socket_file_descriptor, (struct sockaddr *)&their_addr, &sin_size)) == -1){
+            perror("accept");
+            continue;
+        }
+        
+        printf("server: conexion entrante de: %s\n", inet_ntoa(their_addr.sin_addr));
 
-  if(!fork())
-  {
-   while(1)
-   {
-   if (send(new_fd, "Hello, world!\n", 14, 0) == -1) perror("send");
-   else printf("Enviado\n");
-   if ((numbytes=recv(new_fd, buf, MAXDATASIZE, 0)) == -1)
-   {
-    perror("recv");
-    exit(1);
-   }
+        if(!fork()){
+            while(1){
+                if (send(new_file_descriptor, "Hablando desde servidor\n", 25, 0) == -1){
 
-   buf[numbytes] = '\0';
+                perror("send");
+                }
 
-   printf("Received: %s\n", buf);
-  }
-  }  
-  else close(new_fd);
+                else printf("Mensaje enviado\n");
+            
+                if ((bytes_recieved=recv(new_file_descriptor, message, MAXDATASIZE, 0)) == -1){
+                    perror("recv");
+                    exit(1);
+                }
 
-  while(waitpid(-1, NULL, WNOHANG) > 0);
- }
+                message[bytes_recieved] = '\0';
+
+                printf("Recibido: %s\n", message);
+            }
+        }  
+        else close(new_file_descriptor);
+
+        while(waitpid(-1, NULL, WNOHANG) > 0);
+        
+    }
+
+    return 0;
 }
